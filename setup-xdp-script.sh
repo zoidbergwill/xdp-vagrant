5c643373f23ac1ab4e2895c0c22a28dd2369bef9
Paul Chaignon <paul.chaignon@gmail.com>
2016-09-19 11:15:05 +0200

    Install pyroute2 in Vagrantfile

---
+++ setup-xdp-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ "$USER" != "root" ]]; then
+  echo "script must run as root"
+  exit 1
+fi
+
+set -eux
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get install -y python-pip
+pip install pyroute2
