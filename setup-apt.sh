5e827de5bc05e55d19e96789d12fb2361fb4d748
Paul Chaignon <paul.chaignon@gmail.com>
2016-09-26 14:24:56 +0200

    Install required package for add-apt-repository

--- setup-apt.sh
+++ setup-apt.sh
@@ -13,6 +13,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D4284CDD
 echo "deb [trusted=yes] http://52.8.15.63/apt trusty kernel" | tee /etc/apt/sources.list.d/iovisor.list
 echo "deb [trusted=yes] http://52.8.15.63/apt/trusty trusty-nightly main" | tee -a /etc/apt/sources.list.d/iovisor.list
 
+apt-get install -y software-properties-common
 add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
 
 apt-get update
